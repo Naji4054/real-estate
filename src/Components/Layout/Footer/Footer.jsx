ebb4c54d26a8809a7d40ca7fb1a5492db0666200
Naji4054 <mhdnaji4054@gmail.com>
2025-08-19 12:59:53 +0530

    footer text color added

--- src/Components/Layout/Footer/Footer.jsx
+++ src/Components/Layout/Footer/Footer.jsx
@@ -7,17 +7,17 @@ const Footer = () => {
                     <img src="/images/brand-logo.png" alt="brand-logo" />
                 </div>
                 <div className="mb-3">
-                    <p>Lorem Ipsum is simply dummy text of the and typesetting industry. Lorem Ipsum is dummy text of the printing.
+                    <p className="text-[#d7d4d4]">Lorem Ipsum is simply dummy text of the and typesetting industry. Lorem Ipsum is dummy text of the printing.
                     </p>
                 </div>
                 <div className="mb-3">
-                    <p className="flex items-center gap-2"><span><img src="/images/location-pin.png" alt="location-pin" /></span>Brooklyn, New York, United States</p>
+                    <p className="flex items-center gap-2 text-[#d7d4d4]"><span><img src="/images/location-pin.png" alt="location-pin" /></span>Brooklyn, New York, United States</p>
                 </div>
                 <div className="mb-3">
-                    <p className="flex items-center gap-2 "><span><img src="/images/phone-icon.png" alt="location-pin" /></span>+0123-456789</p>
+                    <p className="flex items-center gap-2 text-[#d7d4d4]"><span><img src="/images/phone-icon.png" alt="location-pin" /></span>+0123-456789</p>
                 </div>
                 <div className="mb-3">
-                    <p className="flex items-center gap-2"><span><img src="/images/mail-icon.png" alt="location-pin" /></span>example@example.com</p>
+                    <p className="flex items-center gap-2 text-[#d7d4d4]"><span><img src="/images/mail-icon.png" alt="location-pin" /></span>example@example.com</p>
                 </div>
             </div>
 
@@ -27,12 +27,12 @@ const Footer = () => {
                 </div>
                 <div>
                     <ul>
-                        <li className="mb-3">About</li>
-                        <li className="mb-3">Blog</li>
-                        <li className="mb-3">All Products</li>
-                        <li className="mb-3">Locations Map</li>
-                        <li className="mb-3">FAQ</li>
-                        <li className="mb-3">Contact Us</li>
+                        <li className="mb-3 text-[#d7d4d4]">About</li>
+                        <li className="mb-3 text-[#d7d4d4]">Blog</li>
+                        <li className="mb-3 text-[#d7d4d4]">All Products</li>
+                        <li className="mb-3 text-[#d7d4d4]">Locations Map</li>
+                        <li className="mb-3 text-[#d7d4d4]">FAQ</li>
+                        <li className="mb-3 text-[#d7d4d4]">Contact Us</li>
                     </ul>
                 </div>
             </div>
@@ -42,12 +42,12 @@ const Footer = () => {
                 </div>
                 <div>
                     <ul>
-                        <li className="mb-3">Order Tracking</li>
-                        <li className="mb-3">Wish List</li>
-                        <li className="mb-3">Login</li>
-                        <li className="mb-3">My Account</li>
-                        <li className="mb-3">Terms $ Conditions</li>
-                        <li className="mb-3">Promotional Offers</li>
+                        <li className="mb-3 text-[#d7d4d4]">Order Tracking</li>
+                        <li className="mb-3 text-[#d7d4d4]">Wish List</li>
+                        <li className="mb-3 text-[#d7d4d4]">Login</li>
+                        <li className="mb-3 text-[#d7d4d4]">My Account</li>
+                        <li className="mb-3 text-[#d7d4d4]">Terms $ Conditions</li>
+                        <li className="mb-3 text-[#d7d4d4]">Promotional Offers</li>
                     </ul>
                 </div>
             </div>
@@ -57,12 +57,12 @@ const Footer = () => {
                 </div>
                 <div>
                     <ul>
-                        <li className="mb-3">Login</li>
-                        <li className="mb-3">My account</li>
-                        <li className="mb-3">Wish List</li>
-                        <li className="mb-3">Order tracking</li>
-                        <li className="mb-3">FAQ</li>
-                        <li className="mb-3">Contact Us</li>
+                        <li className="mb-3 text-[#d7d4d4]">Login</li>
+                        <li className="mb-3 text-[#d7d4d4]">My account</li>
+                        <li className="mb-3 text-[#d7d4d4]">Wish List</li>
+                        <li className="mb-3 text-[#d7d4d4]">Order tracking</li>
+                        <li className="mb-3 text-[#d7d4d4]">FAQ</li>
+                        <li className="mb-3 text-[#d7d4d4]">Contact Us</li>
                     </ul>
                 </div>
             </div>
@@ -71,11 +71,11 @@ const Footer = () => {
                     <h4 className="text-[21px] mb-5">News Letter</h4>
                 </div>
                 <div className="mb-3">
-                    <p className="mb-3">Subscribe to our weekly Newsletter and receive updates via email.</p>
-                    <span className="flex items-center"><input  className="flex basis-[85%] min-h-[40px]" type="email" placeholder="Email*" /><img src="/images/messenger.png" alt="messenger" /></span>
+                    <p className="mb-3 text-[#d7d4d4]">Subscribe to our weekly Newsletter and receive updates via email.</p>
+                    <span className="flex items-center text-[#d7d4d4]"><input  className="flex basis-[85%] min-h-[40px]" type="email" placeholder="Email*" /><img src="/images/messenger.png" alt="messenger" /></span>
                 </div>
                 <div>
-                <h4 className="mb-3">We Accept</h4>
+                <h4 className="mb-3 text-[#d7d4d4]">We Accept</h4>
                 <img src="/images/payment-options.png" alt="payment" />
                 </div>
             </div>
