4400700b070ae900500cf2e5602bbe5c195d7499
Naji4054 <mhdnaji4054@gmail.com>
2025-09-16 23:53:32 +0530

    integrated latestproperties b ackend

--- src/Pages/Home/Partners.jsx
+++ src/Pages/Home/Partners.jsx
@@ -4,11 +4,11 @@ const Partners = () => {
   return (
     <section className='mb-[10px]'>
        <div className='flex justify-between  items-center shadow-[0px_14px_21px_11px_whitesmoke]'>
-            <div><img src="/images/partner-1.png" alt="partner1" /></div>
-            <div><img src="/images/partner-2.png" alt="partner2" /></div>
-            <div><img src="/images/partner-3.png" alt="partner3" /></div>
-            <div><img src="/images/partner-4.png" alt="partner4" /></div>
-            <div><img src="/images/partner-5.png" alt="partner5" /></div>
+            <div><img src="/images/partner-1.png" alt="partner1" className='w-[250px] h-[142px]'/></div>
+            <div><img src="/images/partner-2.png" alt="partner2" className='w-[250px] h-[142px]' /></div>
+            <div><img src="/images/partner-3.png" alt="partner3" className='w-[250px] h-[142px]'/></div>
+            <div><img src="/images/partner-4.png" alt="partner4" className='w-[250px] h-[142px]' /></div>
+            <div><img src="/images/partner-5.png" alt="partner5" className='w-[250px] h-[142px]'/></div>
        </div>
     </section>
   )
