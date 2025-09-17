deecb6f7594f1c5749cfd7b210537304b30e9074
Naji4054 <mhdnaji4054@gmail.com>
2025-09-17 18:27:54 +0530

    in progress

--- src/Components/Layout/Header/Header.jsx
+++ src/Components/Layout/Header/Header.jsx
@@ -44,9 +44,6 @@ import { AuthContext } from "../../../Pages/Context/AuthContext";
             </div>
 
             <div className="flex item gap-2">
-                {/* <div>
-                    <img src="/images/user-icon.png" alt="user" />
-                </div> */}
                 <div>
                     <button className="border border-solid border-[#ff5a3c] text-[white] p-[8px_18px] bg-[#ff5a3c] rounded-[5px]">ADD LISTING</button>
                 </div>
