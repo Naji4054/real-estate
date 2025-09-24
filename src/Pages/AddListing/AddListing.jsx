7976e9a97a0762a674161c1e44e4c6d38cd34e2d
Naji4054 <mhdnaji4054@gmail.com>
2025-09-24 10:56:13 +0530

    add listing made into four forms

--- src/Pages/AddListing/AddListing.jsx
+++ src/Pages/AddListing/AddListing.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import axios from 'axios'
 import { PencilIcon } from '@heroicons/react/24/solid'
+import { useState } from 'react'
 
 const AddListing = () => {
+
+  // media upload
+
+  const [uploadedFiles, setUploadedFiles] = useState({
+  })
   
+  const handleSubmit = async (e) => {
+      e.preventDefault()
+      const formData = new FormData()
+
+      Object.keys(uploadedFiles).map(key=> {
+          formData.append(key, uploadedFiles[key])
+      })
+      
+      try {
+          const res = await axios.post('http://localhost:3000/api/test/upload', formData);
+          console.log(res.data);
+      } catch (err) {
+          console.error(err);
+      }
+  }
+  
+  const handleFileChange = (e)=> {
+      const { name, files } = e.target
+      
+      setUploadedFiles(prev=> ({...prev, [name]: files[0]}))
+  }
+
   return (
     <section className='mb-[100px]'>
       <div className="account-bg-image mb-24">
@@ -70,96 +99,112 @@ const AddListing = () => {
               </select>
             </div>
           </div>
-          
+          <div>
+            <button className="border border-solid border-[#ff5a3c] text-[white] p-[8px_18px] bg-[#ff5a3c] w-[300px]">Next</button>
+          </div>
+        </form>  
           
           {/* media section */}
 
-          <div className='mb-[50px]'>
-            <p className='mb-5 text-[18px] font-semibold'>Photos  /  Videos</p>
-            <div className='grid grid-cols-2 gap-8'>
-            <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="text" placeholder='upload image max-size: 5 mb'/>
-            <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="text" placeholder='upload image max-size: 5 mb'/>
-            <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="text" placeholder='upload image max-size: 5 mb'/>
-            <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="text" placeholder='upload image max-size: 5 mb'/>
+        <form onSubmit={handleSubmit} method='POST'>
+            <div className='mb-[50px]'>
+                <p className='mb-5 text-[18px] font-semibold'>Photos  /  Videos</p>
+                <div className='grid grid-cols-2 gap-8'>
+                    <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="file" name='field1' onChange={handleFileChange} placeholder='upload image max-size: 5 mb'/>
+                    <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="file" name='field2' onChange={handleFileChange} placeholder='upload image max-size: 5 mb'/>
+                    <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="file" name='field3' onChange={handleFileChange} placeholder='upload image max-size: 5 mb'/>
+                    <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="file" name='field4' onChange={handleFileChange} placeholder='upload image max-size: 5 mb'/>
+                </div>
             </div>
-          </div>
+            <div>
+              <button className="border border-solid border-[#ff5a3c] text-[white] p-[8px_18px] bg-[#ff5a3c] w-[300px]">Next</button>
+            </div>
+        </form>
 
           {/* location section */}
-          <div className='mb-[50px]'>
-            <p className='mb-5 text-[18px] font-semibold'>Location</p>
-            <div className='grid grid-cols-2 gap-8'>
-              <input  className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="number" placeholder='Latitude'/>
-              <input  className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="number" placeholder='Longitude'/>
+
+        <form>
+            <div className='mb-[50px]'>
+              <p className='mb-5 text-[18px] font-semibold'>Location</p>
+              <div className='grid grid-cols-2 gap-8'>
+                <input  className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="number" placeholder='Latitude'/>
+                <input  className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="number" placeholder='Longitude'/>
+              </div>
             </div>
-          </div>
+            <div>
+              <button className="border border-solid border-[#ff5a3c] text-[white] p-[8px_18px] bg-[#ff5a3c] w-[300px]">Next</button>
+            </div>
+        </form>
+
+          {/* details and ameninties */}
+
+        <form>
 
           {/* details section */}
           <div className='mb-[50px]'>
-          <p className='mb-5 text-[18px] font-semibold'>Details</p>
-          <div className='grid grid-cols-5 gap-8'>
-            <div className='relative'>
-              <input className='w-full border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px] pr-10' type="number" placeholder='Bedroom'/>
-              <PencilIcon className='absolute top-[33%] right-4 h-5 w-5 text-[#ff5a3c] ' />
-            </div>
-            <div className='relative'>
-              <input className='w-full border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px] pr-10' type="number" placeholder='Washroom' />
-              <PencilIcon className='absolute top-[33%] right-4 h-5 w-5 text-[#ff5a3c] ' />
-            </div>
-            <div className='relative'>
-              <input className='w-full border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px] pr-10' type="number" placeholder='Area in Sqft' />
-              <PencilIcon className='absolute top-[33%] right-4 h-5 w-5 text-[#ff5a3c] ' />
-            </div>
-            <div className='relative'>
-              <input className='w-full border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px] pr-10' type="number" placeholder='Garages' />
-              <PencilIcon className='absolute top-[33%] right-4 h-5 w-5 text-[#ff5a3c]' />
-            </div>
-            <div className='relative'>
-              <input className='w-full border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px] pr-10' type="text" placeholder='Garage Size in Sqft' />
-              <PencilIcon className='absolute top-[33%] right-4 h-5 w-5 text-[#ff5a3c]' />
-            </div>
-          </div>
+            <p className='mb-5 text-[18px] font-semibold'>Details</p>
+              <div className='grid grid-cols-5 gap-8'>
+                <div className='relative'>
+                  <input className='w-full border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px] pr-10' type="number" placeholder='Bedroom'/>
+                  <PencilIcon className='absolute top-[33%] right-4 h-5 w-5 text-[#ff5a3c] ' />
+                </div>
+                <div className='relative'>
+                  <input className='w-full border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px] pr-10' type="number" placeholder='Washroom' />
+                  <PencilIcon className='absolute top-[33%] right-4 h-5 w-5 text-[#ff5a3c] ' />
+                </div>
+                <div className='relative'>
+                  <input className='w-full border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px] pr-10' type="number" placeholder='Area in Sqft' />
+                  <PencilIcon className='absolute top-[33%] right-4 h-5 w-5 text-[#ff5a3c] ' />
+                </div>
+                <div className='relative'>
+                  <input className='w-full border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px] pr-10' type="number" placeholder='Garages' />
+                  <PencilIcon className='absolute top-[33%] right-4 h-5 w-5 text-[#ff5a3c]' />
+                </div>
+                <div className='relative'>
+                  <input className='w-full border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px] pr-10' type="text" placeholder='Garage Size in Sqft' />
+                  <PencilIcon className='absolute top-[33%] right-4 h-5 w-5 text-[#ff5a3c]' />
+                </div>
+              </div>
           </div>
 
           {/* aminities section */}
 
           <div className='mb-[50px]'>
-          <p className='mb-5 text-[18px] font-semibold'>Amenities</p>
-
-          <div className='grid grid-cols-4 gap-8'>
-            <div className="flex items-center space-x-2">
-              <input type="checkbox" id="furnished" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
-              <label htmlFor="furnished" className="text-gray-900">Furnished</label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <input type="checkbox" id="kitchen" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
-              <label htmlFor="kitchen" className="text-gray-900">Kitchen</label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <input type="checkbox" id="backyard" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
-              <label htmlFor="backyard" className="text-gray-900">BackYard</label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <input type="checkbox" id="swimming-pool" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
-              <label htmlFor="swimming-pool" className="text-gray-900">Swimming pool</label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <input type="checkbox" id="ventilation" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
-              <label htmlFor="ventilation" className="text-gray-900">Ventilation</label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <input type="checkbox" id="fireplace" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
-              <label htmlFor="fireplace" className="text-gray-900">Fireplace</label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <input type="checkbox" id="elevator" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
-              <label htmlFor="elevator" className="text-gray-900">Elavator</label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <input type="checkbox" id="elevator" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
-              <label htmlFor="elevator" className="text-gray-900">Garage Attached</label>
-            </div>
-          </div>
-
+            <p className='mb-5 text-[18px] font-semibold'>Amenities</p>
+              <div className='grid grid-cols-4 gap-8'>
+                <div className="flex items-center space-x-2">
+                  <input type="checkbox" id="furnished" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
+                  <label htmlFor="furnished" className="text-gray-900">Furnished</label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <input type="checkbox" id="kitchen" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
+                  <label htmlFor="kitchen" className="text-gray-900">Kitchen</label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <input type="checkbox" id="backyard" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
+                  <label htmlFor="backyard" className="text-gray-900">BackYard</label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <input type="checkbox" id="swimming-pool" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
+                  <label htmlFor="swimming-pool" className="text-gray-900">Swimming pool</label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <input type="checkbox" id="ventilation" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
+                  <label htmlFor="ventilation" className="text-gray-900">Ventilation</label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <input type="checkbox" id="fireplace" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
+                  <label htmlFor="fireplace" className="text-gray-900">Fireplace</label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <input type="checkbox" id="elevator" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
+                  <label htmlFor="elevator" className="text-gray-900">Elavator</label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <input type="checkbox" id="elevator" className="h-5 w-5 text-[#ff5a3c] focus:ring-[#ff5a3c] border-gray-300 rounded" />
+                  <label htmlFor="elevator" className="text-gray-900">Garage Attached</label>
+                </div>
+              </div>
           </div>
           <div className="flex justify-between items-center gap-2 ">
                 <button className="border border-solid border-[#ff5a3c] text-[white] p-[8px_18px] bg-[#ff5a3c]">Cancel</button>
--- src/Pages/test/fileUpload.jsx
+++ src/Pages/test/fileUpload.jsx
@@ -1,59 +1,55 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
+// import React, { useEffect, useState } from 'react'
 
-const FileUpload = () => {
-    const [file, setFile] = useState([])
-
-    const [uploadedFiles, setUploadedFiles] = useState({
-      
-    })
+// const FileUpload = () => {
     
-   
-   
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const formData = new FormData()
+//     const [uploadedFiles, setUploadedFiles] = useState({
+//     })
 
-        Object.keys(uploadedFiles).map(key=> {
-            formData.append(key, uploadedFiles[key])
-        })
+//     const handleSubmit = async (e) => {
+//         e.preventDefault()
+//         const formData = new FormData()
+
+//         Object.keys(uploadedFiles).map(key=> {
+//             formData.append(key, uploadedFiles[key])
+//         })
         
-        try {
-            const res = await axios.post('http://localhost:3000/api/test/upload', formData);
-            console.log(res.data);
-        } catch (err) {
-            console.error(err);
-        }
-    }
+//         try {
+//             const res = await axios.post('http://localhost:3000/api/test/upload', formData);
+//             console.log(res.data);
+//         } catch (err) {
+//             console.error(err);
+//         }
+//     }
     
-    const handleFileChange = (e)=> {
-        const { name, files } = e.target
+//     const handleFileChange = (e)=> {
+//         const { name, files } = e.target
         
-        setUploadedFiles(prev=> ({...prev, [name]: files[0]}))
-    }
+//         setUploadedFiles(prev=> ({...prev, [name]: files[0]}))
+//     }
 
-    // useEffect(()=> {
-    //     console.log(uploadedFiles, 'uploaded files')
-    // }, [uploadedFiles])
+//     // useEffect(()=> {
+//     //     console.log(uploadedFiles, 'uploaded files')
+//     // }, [uploadedFiles])
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit} method='POST'>
-        <div className='mb-[50px]'>
-            <p className='mb-5 text-[18px] font-semibold'>Photos  /  Videos</p>
-            <div className='grid grid-cols-2 gap-8'>
-                <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="file" name='field1' onChange={handleFileChange} placeholder='upload image max-size: 5 mb'/>
-                <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="file" name='field2' onChange={handleFileChange} placeholder='upload image max-size: 5 mb'/>
-                <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="file" name='field3' onChange={handleFileChange} placeholder='upload image max-size: 5 mb'/>
-                <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="file" name='field4' onChange={handleFileChange} placeholder='upload image max-size: 5 mb'/>
-                <div>
-                    <button className='border-black border-[1px]'>Submit</button>
-                </div>
-            </div>
-        </div>
-      </form>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit} method='POST'>
+//         <div className='mb-[50px]'>
+//             <p className='mb-5 text-[18px] font-semibold'>Photos  /  Videos</p>
+//             <div className='grid grid-cols-2 gap-8'>
+//                 <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="file" name='field1' onChange={handleFileChange} placeholder='upload image max-size: 5 mb'/>
+//                 <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="file" name='field2' onChange={handleFileChange} placeholder='upload image max-size: 5 mb'/>
+//                 <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="file" name='field3' onChange={handleFileChange} placeholder='upload image max-size: 5 mb'/>
+//                 <input className='border border-solid border-[#e4ecf2] px-[15px] py-[10px] min-h-[65px]' type="file" name='field4' onChange={handleFileChange} placeholder='upload image max-size: 5 mb'/>
+//                 <div>
+//                     <button className='border-black border-[1px]'>Submit</button>
+//                 </div>
+//             </div>
+//         </div>
+//       </form>
+//     </div>
+//   )
+// }
 
-export default FileUpload
+// export default FileUpload
